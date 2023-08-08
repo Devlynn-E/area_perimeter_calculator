dacb0992ff9825dd69a4ebfc88cff96e1cd003db
Devlynn-E <141595269+Devlynn-E@users.noreply.github.com>
2023-08-09 10:48:28 +1200

    Add files via upload

---
+++ looping_basic.py
@@ -0,0 +1,12 @@
+valid = False
+while not valid:
+
+    response = float(input("enter a number: "))
+
+    if response > 0:
+        valid = True
+
+    else:
+        print("please input a number above zero you knob")
+        print()
+
