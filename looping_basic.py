090abdf154a6421bfa7fb41d8e0c90289156eabb
Devlynn-E <141595269+Devlynn-E@users.noreply.github.com>
2023-08-11 14:32:29 +1200

    Add files via upload

--- looping_basic.py
+++ looping_basic.py
@@ -1,18 +1,56 @@
-valid = False
-while not valid:
+def num_check(question):
 
-    error = "please input a number above zero you knob"
+    valid = False
+    while not valid:
 
-    try:
+        error = "please input a number above zero you knob"
 
-        response = float(input("enter a number: "))
+        try:
 
-        if response > 0:
-            valid = True
+            response = float(input(question))
 
-        else:
+            if response > 0:
+                return response
+
+            else:
+                print(error)
+                print()
+
+        except ValueError:
             print(error)
-            print()
 
-    except ValueError:
-        print(error)
+    valid = False
+    while not valid:
+
+        error = "please input a number above zero you knob"
+
+        try:
+
+            length = float(input("enter the length: "))
+
+            if length > 0:
+                valid = True
+
+            else:
+                print(error)
+                print()
+
+        except ValueError:
+            print(error)
+
+
+width = num_check("width: ")
+height = num_check("height: ")
+print()
+print("width", width)
+print("height:", height)
+print()
+
+
+perimeter = 2 * (width + height)
+area = width * height
+
+print("the perimeter is: {}".format(perimeter))
+print()
+print("the area is: {}".format(area))
+print()
